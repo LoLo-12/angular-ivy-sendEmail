67aa091a3fedb5b93b706129f222e4e83d6cf2c2
LoLo-12 <pai0102@hotmail.com>
2021-03-02 16:41:22 +0800

    5 files modified

--- src/app/smptjs/smptjs.component.ts
+++ src/app/smptjs/smptjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+declare let Email: any;
 
 @Component({
   selector: "app-smptjs",
@@ -6,7 +7,24 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./smptjs.component.css"]
 })
 export class SmptjsComponent implements OnInit {
+  token = "2f1ff130-377f-4046-9d0b-df7876d0e122";
+  form = {
+    name: "",
+    email: "",
+    message: ""
+  };
   constructor() {}
 
   ngOnInit() {}
+
+  // https://www.smtpjs.com/
+  public sendForm() {
+    Email.send({
+      SecureToken: "C973D7AD-F097-4B95-91F4-40ABC5567812",
+      To: "them@website.com",
+      From: "you@isp.com",
+      Subject: "This is the subject",
+      Body: "And this is the body"
+    }).then(message => alert(message));
+  }
 }
